3002149522e3f2963cc2a8b88a1cebbb072918b4
Sebastian Fals <77508405+Sebastian-Fals@users.noreply.github.com>
2024-05-25 15:26:17 -0500

    prevent default

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -18,7 +18,12 @@ function TaskForm() {
   };
 
   return (
-    <form className="flex flex-row items-center justify-center">
+    <form
+      className="flex flex-row items-center justify-center"
+      onSubmit={(Event) => {
+        Event.preventDefault();
+      }}
+    >
       <input
         className="h-auto w-2/4 rounded-lg border-2 bg-slate-100 p-3 text-3xl hover:border-green-300 focus:border-none"
         type="text"

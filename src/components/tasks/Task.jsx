52a0ef1231d0af19b631db3d381d886359160853
Sebastian Fals <77508405+Sebastian-Fals@users.noreply.github.com>
2024-05-27 14:20:06 -0500

    Update Task.jsx

--- src/components/tasks/Task.jsx
+++ src/components/tasks/Task.jsx
@@ -4,7 +4,7 @@ import DeleteButton from "./components/DeleteButton";
 function Task({ task, id }) {
   return (
     <section className="relative my-2 flex h-12 w-11/12 flex-row items-center rounded-md bg-gray-50">
-      <h3 className="absolute left-2">{task}</h3>
+      <p className="absolute left-2">{task}</p>
       <DeleteButton id={id}></DeleteButton>
     </section>
   );

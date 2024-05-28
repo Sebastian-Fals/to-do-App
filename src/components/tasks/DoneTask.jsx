9118f7c3b5f2952feef5040bd5b80c1763aaa41a
Sebastian Fals <77508405+Sebastian-Fals@users.noreply.github.com>
2024-05-27 19:09:21 -0500

    BugFixes#3

--- src/components/tasks/DoneTask.jsx
+++ src/components/tasks/DoneTask.jsx
@@ -3,16 +3,24 @@ import Task from "./Task";
 
 function DoneTask() {
   const allTasks = JSON.parse(localStorage.getItem("Tasks"));
-  return (
-    <article className="mx-2 flex w-1/3 flex-col items-center justify-start rounded-lg bg-red-300 pb-5">
-      <h2 className="block text-2xl">Done taks</h2>
-      {allTasks.map((e) => {
-        if (e.state == 2) {
-          return <Task key={e.id} task={e.task} id={e.id}></Task>;
-        }
-      })}
-    </article>
-  );
+  if (allTasks == null) {
+    return (
+      <article className="mx-2 flex w-1/3 flex-col items-center justify-start rounded-lg bg-red-300 pb-5">
+        <h2 className="block text-2xl">Done taks</h2>
+      </article>
+    );
+  } else {
+    return (
+      <article className="mx-2 flex w-1/3 flex-col items-center justify-start rounded-lg bg-red-300 pb-5">
+        <h2 className="block text-2xl">Done taks</h2>
+        {allTasks.map((e) => {
+          if (e.state == 2) {
+            return <Task key={e.id} task={e.task} id={e.id}></Task>;
+          }
+        })}
+      </article>
+    );
+  }
 }
 
 export default DoneTask;
